735a3f89daaffd74b6c47e1c5d341c5b31d2660a
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-17 21:52:04 -0300

    Página inicial do app

---
+++ lib/screens/categories_screens.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:receitas_app/components/category_itens.dart';
+import 'package:receitas_app/data/dummy_data.dart';
+
+class CategoriesScreens extends StatelessWidget {
+  const CategoriesScreens({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          'Let\'s cook?',
+          style: TextStyle(fontSize: 25),
+        )),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItens(
+            category: cat,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
