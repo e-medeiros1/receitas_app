0b1eb4b64eaccbf71635f6c11dad3a9546b62f4c
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-17 21:51:21 -0300

    Dados mockados do projeto

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+const DUMMY_CATEGORIES = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick and Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Fit and Health',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfeast',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.orange,
+  ),
+];
