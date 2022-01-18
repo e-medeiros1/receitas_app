c256a5470976b879fc68dbc9c80e6e0532f4fb97
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-17 23:58:34 -0300

    Mudando os valores de texto pros parametros certos

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:receitas_app/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   const CategoriesMealsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)?.settings.arguments as Category;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Receitas'),
+        title: Text(category.title),
       ),
-      body: const Center(child: Text('Receitas por categoria')),
+      body: Center(
+        child: Text('Receitas por categoria ${category.id}'),
+      ),
     );
   }
 }
