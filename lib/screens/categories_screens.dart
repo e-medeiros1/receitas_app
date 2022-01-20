26c865f5890d362fda2839df90f8c96928916f0c
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-19 23:23:36 -0300

    Implementação da tela de favoritos

--- lib/screens/categories_screens.dart
+++ lib/screens/categories_screens.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:receitas_app/components/category_item.dart';
+
 import 'package:receitas_app/data/dummy_data.dart';
 
 class CategoriesScreens extends StatelessWidget {
@@ -7,28 +8,19 @@ class CategoriesScreens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-            child: Text(
-          'Let\'s cook?',
-          style: TextStyle(fontSize: 24),
-        )),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 18,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES.map((cat) {
-          return CategoryItens(
-            category: cat,
-          );
-        }).toList(),
-      ),
+      children: DUMMY_CATEGORIES.map((cat) {
+        return CategoryItens(
+          category: cat,
+        );
+      }).toList(),
     );
   }
 }
