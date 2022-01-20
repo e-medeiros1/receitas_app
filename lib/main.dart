fb2d42421c84acd5a52cbdd24c2ea2ef495578d9
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-19 23:22:25 -0300

    Tentativas de implantação do package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:receitas_app/components/side_bar_page.dart';
 import 'package:receitas_app/screens/categories_meals_screen.dart';
-import 'package:receitas_app/screens/categories_screens.dart';
+
 import 'package:receitas_app/screens/meal_detail_screen.dart';
+import 'package:receitas_app/screens/tabs_screen.dart';
 import 'package:receitas_app/utils/app_routes.dart';
 
 void main(List<String> args) {
@@ -27,11 +29,11 @@ class MyApp extends StatelessWidget {
             )),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => const CategoriesScreens(),
+
+        AppRoutes.HOME: (ctx) => const TabsScreen(),
         AppRoutes.CATEGORIE_MEALS: (ctx) => const CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => const MealDetailScreen(),
       },
-      
     );
   }
 }
