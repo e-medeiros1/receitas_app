5c5d1e95ec752a837f335c466c9bf26c289ed13e
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-19 23:23:56 -0300

    Gerenciador de abas do app

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:receitas_app/components/main_drawer.dart';
+import 'package:receitas_app/components/side_bar_page.dart';
+import 'package:receitas_app/screens/categories_screens.dart';
+import 'package:receitas_app/screens/favorite_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Widget> _screens = [
+    const CategoriesScreens(),
+    const FavoriteScreen(),
+  ];
+  final List<String> _titles = [
+    'Categories list',
+    'My favorites',
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          _titles[_selectedScreenIndex],
+          style: const TextStyle(fontSize: 24),
+        )),
+      ),
+      drawer: const MainDrawer(),
+      body: _screens[_selectedScreenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        backgroundColor: Colors.white70,
+        enableFeedback: true,
+        unselectedItemColor: Colors.black54,
+        selectedItemColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedScreenIndex,
+        onTap: _selectScreen,
+        items: const [
+          BottomNavigationBarItem(
+            // backgroundColor: Colors.white70,
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            // backgroundColor: Colors.white70,
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
