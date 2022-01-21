2890eb4c7f946c1a792734fbbe5ebce2406f2d45
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-20 22:57:27 -0300

    Adicionando botão de favorito

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -95,6 +95,12 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pop(meal);
+        },
+        child: const Icon(Icons.favorite),
+      ),
     );
   }
 }
