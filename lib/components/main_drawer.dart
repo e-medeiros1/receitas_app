8cec5b3855333de04c6e0f77e8384dc1a290a1b0
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-20 22:56:56 -0300

    Criando ícones através de widget personalizado

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -1,16 +1,15 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:receitas_app/utils/app_routes.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({Key? key}) : super(key: key);
 
-  Widget _createItem(IconData icon, String label) {
+  Widget _createItem(IconData icon, String label, Function() onTap) {
     return ListTile(
       leading: Icon(
         icon,
         size: 26,
-        color: Colors.white,
+        color: Colors.black,
       ),
       title: Text(
         label,
@@ -18,20 +17,20 @@ class MainDrawer extends StatelessWidget {
             fontFamily: 'RobotoCondensed',
             fontSize: 24,
             fontWeight: FontWeight.bold,
-            color: Colors.white),
+            color: Colors.black),
       ),
-      onTap: () {},
+      onTap: onTap,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: Colors.pink.withOpacity(0.2),
+      backgroundColor: Colors.pink[100],
       child: Column(
         children: [
           Container(
-            color: Colors.pink.withOpacity(0.4),
+            color: Colors.pink,
             height: 120,
             width: double.infinity,
             padding: const EdgeInsets.all(20),
@@ -47,8 +46,17 @@ class MainDrawer extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          _createItem(Icons.restaurant, 'Meals'),
-          _createItem(Icons.settings, 'Settings'),
+          _createItem(
+            Icons.restaurant,
+            'Meals',
+            () => Navigator.of(context).pushReplacementNamed(AppRoutes.HOME),
+          ),
+          _createItem(
+            Icons.settings,
+            'Settings',
+            () =>
+                Navigator.of(context).pushReplacementNamed(AppRoutes.SETTINGS),
+          ),
         ],
       ),
     );
