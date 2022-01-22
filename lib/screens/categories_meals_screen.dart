babed2d4f4af063c88a16b6f69d348e830bb64bd
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-21 23:55:37 -0300

    Passando parâmetro meal

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:receitas_app/components/meals_item.dart';
-import 'package:receitas_app/data/dummy_data.dart';
 import 'package:receitas_app/models/category.dart';
-
+import '../models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({Key? key}) : super(key: key);
+  
+   final List<Meal> meal;
+
+  const CategoriesMealsScreen({Key? key,  required this.meal}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
 
-    final categoryMeals = DUMMY_MEALS.where((meal) {
+    final categoryMeals = meal.where((meal) {
       return meal.categories.contains(category.id);
     }).toList();
 
