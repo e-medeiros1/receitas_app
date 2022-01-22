ac840d2f16dd499c7856cbfa4165129ba7ecee15
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-21 23:56:04 -0300

    Adicionando item na tela de favoritos

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:receitas_app/components/meals_item.dart';
+import 'package:receitas_app/models/meal.dart';
 
 class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({ Key? key }) : super(key: key);
+  const FavoriteScreen({Key? key,  required this.favoriteMeal})
+      : super(key: key);
+
+   final List<Meal> favoriteMeal;
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('My Favorite Meals!'),
-    );
+    if (favoriteMeal.isEmpty) {
+      return Center(
+          child: Text(
+        'You have no favorite selections',
+        style: Theme.of(context).textTheme.headline6,
+      ));
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeal.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            meal: favoriteMeal[index],
+          );
+        },
+      );
+    }
   }
 }
